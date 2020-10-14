9c63f085b1aea16adbba903096cb3c267e232b27
Pablo Laso Cortabitarte <pablo.lasocortabitarte@telefonica.com>
2020-10-14 17:38:14 +0200

    config and functions

--- index.js
+++ index.js
@@ -3,3 +3,37 @@ export const URLSlug = (words) => (
     .replace(/\s+/g, '-')
     .toLowerCase()
 );
+
+export const initials = (name) => (
+  name
+    .split(' ')
+    .map(word => `${word.charAt(0).toUpperCase()}.`)
+    .join('')
+);
+
+const months = [
+  'january',
+  'february',
+  'march',
+  'april',
+  'may',
+  'june',
+  'july',
+  'august',
+  'september',
+  'october',
+  'november',
+  'december'
+];
+
+export const monthName = (place) => (
+  months[place]
+    ? months[place]
+    : 'There are only 12 months!'
+);
+
+export const hasPrefix = (str, prefix) => str.startsWith(prefix)
+
+export const withTwoDecimals = (num) => Math.round(100 * num) / 100
+
+export const twoDigitsNum = num => `0${num}`.substr(-2)
