39fbae21aef8f1e9e355a3964bacc1f81b729e8a
Pablo Laso Cortabitarte <pablo.lasocortabitarte@telefonica.com>
2020-10-14 17:49:35 +0200

    test setup

---
+++ tests/index.test.js
@@ -0,0 +1,25 @@
+import { URLSlug, hasPrefix, initials, monthName, twoDigitsNum, withTwoDecimals } from '../index'
+
+describe('URLSlug', () => {
+  
+})
+
+describe('initials', () => {
+
+})
+
+describe('monthName', () => {
+
+})
+
+describe('hasPrefix', () => {
+
+})
+
+describe('withTwoDecimals', () => {
+
+})
+
+describe('twoDigitsNum', () => {
+
+})
